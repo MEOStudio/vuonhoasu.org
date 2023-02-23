95f0be262061f0cfef549f779b2fc80726f8e759
Nguyen Duy Quang <nguyenduyquang2002@gmail.com>
2023-02-23 10:28:13 +0700

    Finalize base story base...
    Apply data to layout page, add a simple Link tag in story page for quick access and other things...

--- pages/story/index.js
+++ pages/story/index.js
@@ -1,6 +1,10 @@
+import Link from "next/link"
+
 export default function Story() {
   return (
-    <div>Story</div>
+    <div>
+      <Link href='story/le-don-hoc-sinh-moi'>Lễ đón học sinh mới</Link>
+    </div>
   )
 }
 
